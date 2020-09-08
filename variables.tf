e5a61342333eab23cb9d1534fda02796a17d70e4
snow-tfe-user <69755779+snow-tfe-user@users.noreply.github.com>
2020-09-08 12:58:55 +0530

    Create variables.tf

---
+++ variables.tf
@@ -0,0 +1,18 @@
+variable "access_key" {}
+
+variable "secret_key" {}
+
+variable "region" {}
+
+#variable "zones" {
+  #type    = "list"
+ # default = ["us-west-1a", "us-west-1b"]
+#}
+
+variable "instance_port" {
+  type = number
+}
+
+variable "protocol" {
+  default = "http"
+}
